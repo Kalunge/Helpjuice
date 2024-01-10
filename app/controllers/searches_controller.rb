65231e7691e8712fb5496c561e920641fbc6f56b
Titus Muthomi <muthomititus+github@gmail.com>
2024-01-10 10:32:45 +0300

    Add post action and resolve csrf error

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,11 @@
 class SearchesController < ApplicationController
+  protect_from_forgery except: :create
+
   def index
+  end
+
+  def create
+    query = params[:query]
+    puts "Search query enteed is: #{query}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "searches#index"
+  post '/search', to: 'searches#create'
   resources :searches, only: [:index, :create]
 end
